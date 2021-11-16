e1b6ce88cda8463e8b65dc0b0e4a4fa9180d1b70
Alexander <amchops@comcast.net>
2021-11-15 21:04:23 -0800

    Completed prep-javascript-arrays

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,10 @@
+const languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+console.log('languages array', languages);
+var firstElement = 'html';
+console.log(' the first element of the array is', firstElement);
+var thirdElement = 'javascript';
+console.log('the 3rd element of the array is', thirdElement);
+var length = languages.length;
+console.log('The length of the array is', length);
+var lastElement = 'postgres';
+console.log('The last element of the array is', lastElement);
